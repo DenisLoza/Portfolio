096564fde4aa4595b7ba8e3acde9fe92f585ce74
DenisLoza <denisloza.git@gmail.com>
2020-05-21 19:41:15 +0300

    Updates

---
+++ precache-manifest.c6d56f7c9c2c9fe10a1ef2d2504ba9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce8b061226706536831d3b7e4f2c953",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "91d90a881e6e232f5024",
+    "url": "/Portfolio/static/css/main.cf471d0d.chunk.css"
+  },
+  {
+    "revision": "a68cef6106665337cefe",
+    "url": "/Portfolio/static/js/2.cd43da79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.cd43da79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d90a881e6e232f5024",
+    "url": "/Portfolio/static/js/main.571e2dd9.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  }
+]);
